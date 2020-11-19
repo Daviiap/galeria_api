62a7c04a3fa008fa7d00249c6e2386aa4130f27f
Davi Andrade Pontes <daviiap@outlook.com>
2020-11-18 23:48:17 -0300

    Removendo senha

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ const database = {
     env: 'development',
     host: process.env.DB_HOST || 'localhost',
     username: process.env.DB_USERNAME || 'root',
-    password: process.env.DB_PASSWORD || 'davi',
+    password: process.env.DB_PASSWORD || 'root',
     database: process.env.DB_NAME || 'galeria',
     dialect: 'postgres',
     options: {
